93f6ad86546bd0924a4d12c2ea2620803eba29c5
Jivan Valerie <maryam.valerie@gmail.com>
2021-08-17 20:11:28 +0200

    Back end Carousel - link item galerie

--- migrations/Version20210816091848.php
+++ migrations/Version20210816091848.php
@@ -21,13 +21,11 @@ final class Version20210816091848 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE detail_commande ADD image VARCHAR(255) NOT NULL');
-        $this->addSql('ALTER TABLE tableaux RENAME INDEX technique_id TO IDX_307CE1D41F8ACB26');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('ALTER TABLE detail_commande DROP image');
-        $this->addSql('ALTER TABLE tableaux RENAME INDEX idx_307ce1d41f8acb26 TO technique_id');
     }
 }
